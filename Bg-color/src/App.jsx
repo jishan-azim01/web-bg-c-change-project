639412687704ac342106d3e4d26787cf878af469
Jishan Azim <[200~jisanazim56@gmail.com~>
2024-09-15 11:08:26 +0600

    added channel name

--- Bg-color/src/App.jsx
+++ Bg-color/src/App.jsx
@@ -10,23 +10,23 @@ function App() {
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3  py-2 rounded-3xl">
           <button
           onClick={() => setColor("red")}
-          className="outline-none px-4 py-1 rounded-full text-red-700 font-semibold shadow-lg"
+          className="outline-none px-4 py-1 rounded-full bg-red-700 text-white font-semibold shadow-lg"
           style={{backgorundColor: "red"}}>Red</button>
           <button
           onClick={() => setColor("green")}
-          className="outline-none px-4 py-1 rounded-full text-green-700 font-semibold shadow-lg"
+          className="outline-none px-4 py-1 rounded-full bg-green-700 text-white font-semibold shadow-lg"
           style={{backgorundColor: "white"}}>Green</button>
           <button
           onClick={() => setColor("yellow")}
-          className="outline-none px-4 py-1 rounded-full text-yellow-700 font-semibold shadow-lg"
+          className="outline-none px-4 py-1 rounded-full bg-yellow-700 text-white font-semibold shadow-lg"
           style={{backgorundColor: "yellow"}}>Yellow</button>
           <button
           onClick={() => setColor("blue")}
-          className="outline-none px-4 py-1 rounded-full text-blue-700 font-semibold shadow-lg"
+          className="outline-none px-4 py-1 rounded-full bg-blue-700 text-white font-semibold shadow-lg"
           style={{backgorundColor: "Blue"}}>Blue</button>
           <button
           onClick={() => setColor("black")}
-          className="outline-none px-4 py-1 rounded-full text-black-700 font-semibold shadow-lg"
+          className="outline-none px-4 py-1 rounded-full bg-black text-white font-semibold shadow-lg"
           style={{backgorundColor: "black"}}>Black</button>
         </div>
       </div>
